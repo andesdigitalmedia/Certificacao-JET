7e9b93337a11bef21afbcd905118fa153c050374
andesdm <desenvolvimento@andesdm.com.br>
2019-01-23 19:40:26 -0200

    Remocao

--- resources/js/api/customer/contact.js
+++
@@ -1,9 +0,0 @@
-﻿import {isValidEmail} from "../../functions/validate"
-import {_alert,} from "../../functions/message";
-
-$(document).on("click", "#submitContact", function(e){
-    if(!isValidEmail($("#email").val())){
-        _alert("", "Informe um e-mail válido.", "error")
-        return false
-    }
-})
